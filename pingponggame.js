c488011aa083ffe8b48cbcfd5864bfcd334f8356
tomlam <tomlam@sparq-sonlam.att.net>
2016-07-16 00:04:13 -0700

    Moving Ball

--- pingponggame.js
+++ pingponggame.js
@@ -1,11 +1,31 @@
 var canvas;
 var canvasContext;
+var ballX = 400;
+var ballY = 300;
+var radius = 10;
 
 window.onload = function() {
+	
 	console.log("Get Ready to Play!");
 	canvas = document.getElementById('gameCanvas');
-	canvasContext = canvas.getContext('2d');
-	canvasContext.fillStyle = 'navy';
-	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+	canvas.setAttribute('width', 800);
+	canvas.setAttribute('height', 600);
+	canvasContext = canvas.getContext('2d')
 
+	setInterval(drawEverything,10);
+}
+
+function drawEverything()
+{
+	ballX = ballX + (Math.random()*10)-5;
+	ballY = ballY + (Math.random()*10)-5;
+	canvasContext.fillStyle = 'green';
+	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+	canvasContext.beginPath();
+    canvasContext.arc(ballX, ballY, radius, 0, 2 * Math.PI, false);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fill();
+    canvasContext.lineWidth = 0;
+    canvasContext.strokeStyle = '#003300';
+    canvasContext.stroke();
 }
